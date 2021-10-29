c1f32746baf40ffdf6e1ac6c1cf20e3f9136c4b3
LinsIan <jjes20055114123@gmail.com>
2021-10-29 08:13:17 +0800

    Setting project properties

--- 3DGraphicsPractice/3DGraphicsPractice.cpp
+++ 3DGraphicsPractice/3DGraphicsPractice.cpp
@@ -1,21 +1,22 @@
-﻿// 3DGraphicsPractice.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿//opengl2
 
 #include <iostream>
+#include <stdio.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
+#include <GLFW/glfw3.h>
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error");
+}
 
 int main()
 {
-    //把opengl2的資料夾結構引入進來
     std::cout << "Hello World!\n";
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
